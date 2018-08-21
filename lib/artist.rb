46651acb8cf3d63214eb7f51e26d75d9882ca28e
Tammy Metz <tammy@tammymetz.com>
2018-08-21 23:30:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,20 @@
 class Artist
   attr_accessor :name, :songs
+
+  @@all_songs = []
+
+  def add_song(song)
+    song.artist = self
+    @songs << song
+    @@all_songs << song
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+
+  def self.song_count
+    @@all_songs.length
+  end
 end

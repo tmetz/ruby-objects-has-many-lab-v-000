8a026e7c95d338e509c493ff1d8ecc744bcaeac7
Tammy Metz <tammy@tammymetz.com>
2018-08-21 23:37:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   @@all = []
   attr_reader :name
+  attr_writer :artist
 
   def initialize(song_name)
     name = song_name
@@ -9,5 +10,9 @@ class Song
 
   def self.all
     @@all
+  end
+
+  def artist_name
+    @artist.name
   end
 end

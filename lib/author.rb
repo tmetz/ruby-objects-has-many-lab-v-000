557cff948abd9aabdadd59d5390d97f701aed7b7
Tammy Metz <tammy@tammymetz.com>
2018-08-21 23:53:19 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,4 +4,14 @@ class Author
     @name = name
     @posts = []
   end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
 end

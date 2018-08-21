60cedf7aa10bbb1836b7f7b0627443624eb00957
Tammy Metz <tammy@tammymetz.com>
2018-08-21 23:56:21 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,18 @@
 class Post
   @@all = []
-  attr_accessor :title
+  attr_accessor :title, :author
 
   def initialize(title)
     @title = title
     @@all << self
+  end
+
+  def author_name
+    if self.author.name
+      self.author.name
+    else
+      nil
+    end
   end
 
   def self.all
